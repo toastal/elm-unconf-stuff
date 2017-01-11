b5926c67fc098a5ab369ddc72eb0892c298a4c24
Kyle J. Kress <toastal@protonmail.com>
2017-01-10 17:25:35 -0700

    point-free-er

--- src/Main.elm
+++ src/Main.elm
@@ -13,7 +13,7 @@ main : Program Never Model msg
 main =
     Html.program
         { init = Return.singleton ()
-        , update = \_ _ -> Return.singleton ()
+        , update = always Return.singleton
         , view = always <| text "¡Hola, mundo!"
         , subscriptions = always Sub.none
         }
