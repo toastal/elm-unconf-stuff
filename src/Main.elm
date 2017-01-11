145b9932a955e147ca21465ee9155964aa9998d3
Kyle J. Kress <toastal@protonmail.com>
2017-01-10 17:18:26 -0700

    running example

--- src/Main.elm
+++ src/Main.elm
@@ -2,13 +2,18 @@ module Main exposing (main)
 
 import Html exposing (..)
 import Html.Attributes as Attr exposing (..)
+import Return exposing (Return)
 
 
-main : Program Never model msg
+type alias Model =
+    ()
+
+
+main : Program Never Model msg
 main =
     Html.program
-        { init = init
-        , update = update
-        , view = view
-        , subscriptions = subscriptions
+        { init = Return.singleton ()
+        , update = \_ _ -> Return.singleton ()
+        , view = always <| text "¡Hola, mundo!"
+        , subscriptions = always Sub.none
         }
